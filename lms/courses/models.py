987c2d1dc29df2e49f57345ae35c1d1f362a98da
Robert Roskam <robert.roskam@gmail.com>
2015-03-02 23:30:00 -0500

    Cleaned up default values on form.

--- lms/courses/models.py
+++ lms/courses/models.py
@@ -9,18 +9,14 @@ DURATION_CHOICES = ((WEEKS2, '2 Weeks'),
 
 # Create your models here.
 class Course(models.Model):
-
-
     title = models.CharField(max_length=200)
     description = models.TextField(help_text="Provide some information about what students will learn.")
-    instructor = models.CharField(max_length=200,
-                                  default='Django Guru')
+    instructor = models.CharField(max_length=200)
     duration = models.IntegerField(max_length=2, choices=DURATION_CHOICES, default=WEEKS2)
     course_art = models.FileField(upload_to='img',)
 
 class CourseForm(ModelForm):
     duration = forms.ChoiceField(widget=forms.RadioSelect, choices=DURATION_CHOICES)
-    title = forms.CharField(initial='Name it something good')
     class Meta:
         model = Course
         fields = ['title', 'description', 'instructor', 'duration', 'course_art']
