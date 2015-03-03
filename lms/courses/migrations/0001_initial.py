8b256e01ec7181767a34c8bec54bb3ad397ef92a
Robert Roskam <robert.roskam@gmail.com>
2015-03-02 23:49:57 -0500

    Removed from tracking.

--- lms/courses/migrations/0001_initial.py
+++ lms/courses/migrations/0001_initial.py
@@ -13,12 +13,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Course',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(default='Name it something good', max_length=200)),
-                ('description', models.TextField()),
-                ('instructor', models.CharField(default='Django Guru', max_length=200)),
-                ('duration', models.IntegerField(default=2, choices=[(2, '2 Weeks'), (8, '8 Weeks')], max_length=2)),
-                ('course_art', models.FileField(upload_to='')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('title', models.CharField(max_length=200)),
+                ('description', models.TextField(help_text='Provide some information about what students will learn.')),
+                ('instructor', models.CharField(max_length=200)),
+                ('duration', models.IntegerField(max_length=2, default=2, choices=[(2, '2 Weeks'), (8, '8 Weeks')])),
+                ('course_art', models.FileField(upload_to='img')),
             ],
             options={
             },
